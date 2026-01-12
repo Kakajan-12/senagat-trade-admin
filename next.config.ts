3670c993bc0b836bff012e18bbfdbb2f76a8d93d
Kakajan-12 <83004880+Kakajan-12@users.noreply.github.com>
2026-01-12 20:43:41 +0500

    done

--- next.config.ts
+++ next.config.ts
@@ -1,22 +1,22 @@
 module.exports = {
-    // images: {
-    //     remotePatterns: [
-    //         {
-    //             protocol: 'https',
-    //             hostname: 'api.koshibakery.com',
-    //             pathname: '/uploads/**',
-    //         },
-    //     ],
-    // },
     images: {
-        unoptimized: true,
         remotePatterns: [
             {
-                protocol: 'http',
-                hostname: 'localhost',
-                port: '3001',
+                protocol: 'https',
+                hostname: 'api.senagat.com',
                 pathname: '/uploads/**',
             },
         ],
     },
+    // images: {
+    //     unoptimized: true,
+    //     remotePatterns: [
+    //         {
+    //             protocol: 'http',
+    //             hostname: 'localhost',
+    //             port: '3001',
+    //             pathname: '/uploads/**',
+    //         },
+    //     ],
+    // },
 };
